ed11f08e7e761a69c7f6e1fb3905a3cedf3cd20a
Wouter Vernaillen <wouter@vernaillen.com>
2022-05-21 17:44:35 +0200

    fixed test

--- demo/src/components/__tests__/DemoIntro.spec.ts
+++ demo/src/components/__tests__/DemoIntro.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import DemoIntro from '../DemoIntro.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(DemoIntro, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
