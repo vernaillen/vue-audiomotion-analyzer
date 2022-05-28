7c3c6a4d5c088a68c918d106a8f6ed8c432a0f7a
Wouter Vernaillen <wouter@vernaillen.com>
2022-05-28 22:46:34 +0200

    trying to output both docs & demo-ssg builds to one dist-netlify folder

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,5 @@
 export default {
   title: 'Vue AudioMotion Analyzer',
-  description: 'Documentation for Vue AudioMotion Analyzer'
+  description: 'Documentation for Vue AudioMotion Analyzer',
+  outDir: resolve(root, 'netlify-dist'),
 }
