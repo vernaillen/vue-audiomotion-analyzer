ef57bce129a851fe0cab4e0a7851e10f0e42e571
Wouter Vernaillen <wouter@vernaillen.com>
2022-07-19 08:17:53 +0200

    v0.6.2 of plugin, vite 3.0.2 in demos + beat blender stream for demo-ssg

--- examples/demo-ssg/src/components/DemoAnalyzer.vue
+++ examples/demo-ssg/src/components/DemoAnalyzer.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { DefaultOptions } from "vue-audiomotion-analyzer";
 
 const audio = ref<HTMLMediaElement>();
 onMounted(() => {
   audio.value = document.getElementById("audio") as HTMLMediaElement;
 });
+const options = {
+  mode: 5,
+  barSpace: 0.25,
+  gradient: "rainbow",
+  ledBars: false,
+  lumiBars: false,
+  radial: false,
+  reflexAlpha: 0.25,
+  reflexBright: 1,
+  reflexFit: true,
+  reflexRatio: 0.3,
+  showBgColor: false,
+  showPeaks: true,
+  overlay: false,
+};
 </script>
 
 <template>
   <audio
     id="audio"
     ref="audioRef"
-    src="http://ice5.somafm.com/groovesalad-256-mp3"
+    src="https://ice2.somafm.com/beatblender-128-mp3"
     controls
     crossorigin="anonymous"
   ></audio>
@@ -21,11 +35,11 @@ onMounted(() => {
     <button @click="audio?.pause()">Pause</button>
     <br />
     Live stream:
-    <a href="https://somafm.com/groovesalad/" rel="noopener" target="_blank"
-      >Soma FM Groove Salad</a
+    <a href="https://somafm.com/beatblender/" rel="noopener" target="_blank"
+      >Soma FM Beat Blender</a
     >
   </div>
-  <VueAudioMotionAnalyzer :options="DefaultOptions" :source="audio" />
+  <VueAudioMotionAnalyzer :options="options" :source="audio" />
 </template>
 
 <style scoped>
