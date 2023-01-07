8b487bc76b997600e21fcff437791ba49a0bef39
Wouter Vernaillen <wouter@vernaillen.com>
2023-01-07 08:49:17 +0100

    removed old plugin

--- plugin/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/vue-audiomotion-analyzer.prod.cjs')
-} else {
-  module.exports = require('./dist/vue-audiomotion-analyzer.cjs')
-}
