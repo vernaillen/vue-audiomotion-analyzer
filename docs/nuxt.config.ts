7a5ca04ee359cdd205847875c7b336a54b9ebb17
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-06 15:18:56 +0200

    ignore prerender of pinceau_tokens...

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
+
+  nitro: {
+    prerender: {
+      ignore: ['/__pinceau_tokens_config.json', '/__pinceau_tokens_schema.json']
+    }
+  },
 })
