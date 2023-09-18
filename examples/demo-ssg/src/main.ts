42192847376e615de78a7c37f480564883292b24
Wouter Vernaillen <wouter@vernaillen.com>
2023-09-18 08:15:20 +0200

    removed demo-ssg

--- examples/demo-ssg/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { ViteSSG } from 'vite-ssg'
-
-// import { VueAudioMotionAnalyzerPlugin } from 'vue-audiomotion-analyzer'
-import App from './App.vue'
-import routes from './router'
-
-export const createApp = ViteSSG(App, { routes }, () => {
-//  app.use(VueAudioMotionAnalyzerPlugin)
-})
--- examples/demo-ssg/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import DemoAnalyzer from '@/components/DemoAnalyzer.vue'
-</script>
-
-<template>
-  <main>
-    <DemoAnalyzer />
-  </main>
-</template>
